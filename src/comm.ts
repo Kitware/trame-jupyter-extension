77b08c799ff63fce708ac82d980d4bc1687b0c56
Alessandro Genova <alessandro.genova@kitware.com>
2023-11-02 16:57:13 -0400

    fix(jupyter): Rework comm creation to make it compatible w/ other extensions

--- src/comm.ts
+++ src/comm.ts
@@ -2,10 +2,7 @@ import {
   IKernelConnection,
   IComm
 } from '@jupyterlab/services/lib/kernel/kernel';
-import {
-  ICommMsgMsg,
-  ICommCloseMsg
-} from '@jupyterlab/services/lib/kernel/messages';
+import { ICommMsgMsg } from '@jupyterlab/services/lib/kernel/messages';
 
 import { ConcreteEmitter } from './emitter';
 
@@ -16,6 +13,8 @@ export type CommMessage = {
 
 export type CommEvents = {
   message: CommMessage;
+  open: void;
+  close: void;
 };
 
 export class TrameJupyterComm extends ConcreteEmitter<CommEvents> {
@@ -27,10 +26,28 @@ export class TrameJupyterComm extends ConcreteEmitter<CommEvents> {
 
     this.kernel = kernel;
     this.comm = null;
+
+    this.kernel.disposed.connect(this.onClose.bind(this));
+    this.kernel.statusChanged.connect((_kernel, status) => {
+      if (
+        status === 'restarting' ||
+        status === 'autorestarting' ||
+        status === 'terminating' ||
+        status === 'dead'
+      ) {
+        this.onClose();
+      }
+    });
   }
 
   open() {
-    if (!this.comm || this.comm.isDisposed) {
+    if (this.kernel.isDisposed) {
+      throw new Error(
+        `Can't open a comm for disposed kernel ${this.kernel.id}`
+      );
+    }
+
+    if ((!this.comm || this.comm.isDisposed) && !this.kernel.isDisposed) {
       this.comm = this.kernel.createComm('wslink_comm');
       this.comm.open();
       this.comm.onMsg = this.onMessage.bind(this);
@@ -39,8 +56,8 @@ export class TrameJupyterComm extends ConcreteEmitter<CommEvents> {
   }
 
   send(message: CommMessage) {
-    if (this.comm) {
-      this.comm.send(message.data, undefined, message.buffers);
+    if (this.isUseable()) {
+      this.comm!.send(message.data, undefined, message.buffers);
     } else {
       console.error('trame::jupyter-comm::send -- NO COMM');
     }
@@ -50,7 +67,13 @@ export class TrameJupyterComm extends ConcreteEmitter<CommEvents> {
     this.emit('message', { data: msg.content.data, buffers: msg.buffers });
   }
 
-  onClose(msg: ICommCloseMsg<'iopub' | 'shell'>) {
+  onClose(...args: any) {
     console.error('trame::jupyter-comm::close -- NO COMM');
+    this.comm = null;
+    this.emit('close', undefined);
+  }
+
+  isUseable() {
+    return !this.kernel.isDisposed && this.comm && !this.comm.isDisposed;
   }
 }
